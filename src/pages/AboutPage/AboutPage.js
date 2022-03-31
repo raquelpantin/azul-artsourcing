abc5eb555cce0c1fefdb4a456f69e130df8c58ed
Raquel Pantin <raquel.pantin@gmail.com>
2022-03-31 18:40:28 -0400

    added motion to about page

--- src/pages/AboutPage/AboutPage.js
+++ src/pages/AboutPage/AboutPage.js
@@ -3,26 +3,37 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
 import image1 from "../../assets/images/about2.jpg";
 import image2 from "../../assets/images/about1.jpg";
+import { motion } from "framer-motion";
 
 function AboutPage() {
   const navigate = useNavigate();
 
   return (
     <div className="about-page">
-      <div className="about-container">
+      <motion.div
+        initial={{ x: "100%" }}
+        animate={{ x: "calc(50vw - 50%)" }}
+        transition={{ duration: 1 }}
+        className="about-container"
+      >
         <img className="about-image" src={image1} />
         <p className="about-texttop">
           AZUL is an art sourcing platform designed to connect clients and
           artists with the click of a button
         </p>
-      </div>
-      <div className="about-container">
+      </motion.div>
+      <motion.div
+        initial={{ x: "-100%" }}
+        animate={{ x: "calc(-40vw + 50%)" }}
+        transition={{ duration: 1.1 }}
+        className="about-container"
+      >
         <p className="about-textbottom">
           So, whether you need a large-scale mural for the front of your
           business or a digital portrait of your pooch, AZUL has you covered!
         </p>
         <img className="about-image" src={image2} />
-      </div>
+      </motion.div>
       <div className="about-container__bottom">
         <p className="about-cta__header">Ready to get started?</p>
         <p className="about-cta__text">
